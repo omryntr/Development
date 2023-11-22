659ed174ed04dad4d49565fadcfe7269fbc7a4f0
omryntr <omryntr@gmail.com>
2023-11-23 01:40:40 +0300

    Commit_

--- genlib/connectlib/connectlib.go
+++ genlib/connectlib/connectlib.go
@@ -2,11 +2,14 @@ package connectlib
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	_ "github.com/sijms/go-ora"
 )
 
 func Read_ConnectSTR(v_file string) (v_host string, v_port int, v_user string, v_password string, v_dbname string) {
@@ -15,7 +18,6 @@ func Read_ConnectSTR(v_file string) (v_host string, v_port int, v_user string, v
 	if err != nil {
 		panic(err)
 	}
-
 	// Blok sonunda dosyayı kapat
 	defer file.Close()
 
@@ -59,4 +61,58 @@ func Read_ConnectSTR(v_file string) (v_host string, v_port int, v_user string, v
 	}
 
 	return v_host, v_port, v_user, v_password, v_dbname
+}
+
+func Connect_Oracle(password string) (*sql.DB, error) {
+	// Replace these values with your database credentials
+	username := "system"
+	serviceName := "XE"
+
+	// The SSH tunnel is forwarding the remote Oracle port to the local port 2525
+	host := "DESKTOP-5N72M6L.local"
+	port := 1521
+
+	// Set the number of prefetch rows
+	//prefetchRows := 1000
+
+	// Create the connection string
+	connStr := fmt.Sprintf(
+		"oracle://%s:%s@%s:%d/%s",
+		username, password, host, port, serviceName)
+
+	fmt.Println("Opening connection to db using go-ora:")
+	fmt.Println(connStr)
+
+	// Connect to the database
+	db, err := sql.Open("oracle", connStr)
+	if err != nil {
+		fmt.Printf("HATA (1: %v", err)
+		return nil, err
+	}
+
+	fmt.Println("HATA ALMADI....")
+	fmt.Println(db)
+
+	rows, err := db.Query("SELECT * FROM dvsys.CODE$")
+	if err != nil {
+		fmt.Println("HATA ALDIK....")
+		fmt.Println(db)
+		return nil, err
+	}
+	defer rows.Close()
+
+	fmt.Println("HATA ALMADI....")
+	fmt.Println(rows)
+	/*
+		// Test the connection
+		err = db.Ping()
+		if err != nil {
+			fmt.Println("HATA ALDIK....")
+			fmt.Println(db)
+			fmt.Println(err)
+			db.Close()
+			return nil, err
+		}
+	*/
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/harry1453/go-common-file-dialog v1.2.0
 require (
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/google/uuid v1.1.1 // indirect
+	github.com/sijms/go-ora v1.3.2
 )
